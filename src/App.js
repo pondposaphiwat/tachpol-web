c99bce61bb5749186d0bd3ae8320238f434e2bde
pondposaphiwat <70335919+pondposaphiwat@users.noreply.github.com>
2024-07-25 21:04:01 -0700

    basic web template
    - mock nav bar from bootstrap
    - mock intro block
    - mock project blocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import myImage from './IMG_2699.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>    
+      <NavBarPond />
+    {/* <div className="App">
+    <h2> Tachpol </h2>
+    </div> */}
+    <IntroBlock imageSrc={myImage} text={
+      "Hi! My name is Pond. I would love to connect!"
+    }/>
+    <div className='image-description-blocks'>
+    <ProjectDescriptionBlock imageSrc={myImage} description={"project"}/>
+    <ProjectDescriptionBlock imageSrc={myImage} description={"project"}/>
+    </div>
+    <div className='image-description-blocks'>
+    <ProjectDescriptionBlock imageSrc={myImage} description={"project"}/>
+    <ProjectDescriptionBlock imageSrc={myImage} description={"project"}/>
+    </div>
+  </div>
+  );
+}
+
+function IntroBlock ({ imageSrc, altText, text }) {
+  return (
+    <div className="image-text-block">
+      <img src={imageSrc} alt={altText} className="image" />
+      <div className="text">
+        {text}
+      </div>
     </div>
   );
+};
+
+function ProjectDescriptionBlock ({ imageSrc, altText, description }) {
+  return (
+    <div className="image-description-block">
+      <img src={imageSrc} alt={altText} className="image-proj" />
+      <div className="description">
+        {description}
+      </div>
+    </div>
+  );
+};
+
+function NavBarPond() {
+  return <>  
+  <Navbar bg="light" data-bs-theme="light">
+  <Container>
+    <Navbar.Brand href="#home"></Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+  </Container>
+  </Navbar></>
 }
 
 export default App;
